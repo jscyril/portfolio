8b249c0756d47faf878d3bb66e0120ccb0a82c06
jscyril <jacobsebcyril@gmail.com>
2024-12-13 14:08:24 +0530

    added responsiveness for mobile devices

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -12,11 +12,9 @@ export default function Navbar() {
       const currentScrollY = window.scrollY;
 
       if (currentScrollY > lastScrollY && currentScrollY > 50) {
-        // Scrolling down and passed 50px
-        setIsVisible(false);
+        setIsVisible(false); // Hide on scroll down
       } else {
-        // Scrolling up
-        setIsVisible(true);
+        setIsVisible(true); // Show on scroll up
       }
 
       setLastScrollY(currentScrollY);
@@ -28,12 +26,12 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`bg-[#14203B] text-white fixed w-full top-0 left-0 z-50 transition-transform duration-300 ${
+      className={`bg-[#14203B] text-white fixed w-full top-0 left-0 z-50 transition-transform duration-300 overflow-hidden ${
         isVisible ? "translate-y-0" : "-translate-y-full"
       }`}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+      <div className="w-full px-4">
+        <div className="flex items-center justify-between h-16 w-full">
           {/* Logo Section */}
           <div className="flex-shrink-0">
             <a href="/" className="flex items-center">
@@ -71,7 +69,7 @@ export default function Navbar() {
           </div>
 
           {/* Mobile Menu Button */}
-          <div className="md:hidden">
+          <div className="md:hidden flex-shrink-0">
             <button
               onClick={() => setIsMenuOpen(!isMenuOpen)}
               className="inline-flex items-center justify-center p-2 rounded-md text-[#6FFFE9] hover:text-white hover:bg-[#1C2541] focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -92,25 +90,26 @@ export default function Navbar() {
         <div className="md:hidden bg-[#0B132B]">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <a
-              href="/about"
+              href="#about"
               className="block px-3 py-2 rounded-md text-base font-medium hover:bg-[#6FFFE9] hover:text-[#0B132B] transition-colors duration-300"
             >
               About
             </a>
             <a
-              href="/projects"
+              href="#projects"
               className="block px-3 py-2 rounded-md text-base font-medium hover:bg-[#6FFFE9] hover:text-[#0B132B] transition-colors duration-300"
             >
               Projects
             </a>
             <a
-              href="/contact"
+              href="#contact"
               className="block px-3 py-2 rounded-md text-base font-medium hover:bg-[#6FFFE9] hover:text-[#0B132B] transition-colors duration-300"
             >
               Contact
             </a>
             <a
-              href="/resume"
+              href="https://drive.google.com/file/d/1RxT7dXFQLgYDUx8LPj2a80C_wVlMW-d5/view?usp=sharing"
+              target="_blank"
               className="block px-3 py-2 rounded-md text-base font-medium border border-[#6FFFE9] text-[#6FFFE9] hover:bg-[#6FFFE9] hover:text-[#0B132B] transition-colors duration-300"
             >
               Resume
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -7,7 +7,7 @@ const projects = [
       "A full-stack application built with React, NodeJS, Express, and PostgreSQL. It features a user authentication system, real-time data updates, and a clean user interface.",
     technologies: ["NodeJS", "Express", "PostgreSQL", "React"],
     githubLink: "https://github.com/jscyril/UniLink",
-    imageUrl: "src/assets/unilink.png", // Replace with actual project image
+    imageUrl: "src/assets/unilink.png",
   },
   {
     title: "HabiTrax",
@@ -15,9 +15,9 @@ const projects = [
       "A mobile app built with React Native and Expo Framework that helps users track their habits.",
     technologies: ["React Native", "NodeJS", "ExpressJS", "MongoDB"],
     githubLink: "https://github.com/jscyril/HabiTrax",
-    imageUrl: "src/assets/habitrax.png", // Replace with actual project image
+    imageUrl: "src/assets/habitrax.png",
   },
-  // Add more projects here
+  // Add more projects as needed
 ];
 
 const ProjectsSection = () => {
@@ -26,10 +26,10 @@ const ProjectsSection = () => {
       id="projects"
       className="bg-[#0B132B] text-white py-20 px-4 lg:py-28 lg:px-0"
     >
-      <div className="w-full mx-[256px]">
+      <div className="max-w-5xl mx-auto">
         {/* Section Header */}
         <motion.h2
-          className="text-4xl lg:text-5xl font-bold text-[#6FFFE9] mb-16 pr-8"
+          className="text-4xl lg:text-5xl font-bold text-[#6FFFE9] mb-12 relative"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
@@ -37,57 +37,66 @@ const ProjectsSection = () => {
         >
           My Projects
         </motion.h2>
+
         {/* Projects Grid */}
-        <div className="flex justify-center mx-auto">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 justify-center max-w-7xl">
-            {projects.map((project, index) => (
-              <motion.div
-                key={index}
-                className="bg-[#1c2330] p-8 pb-16 rounded-xl shadow-xl hover:shadow-2xl relative"
-                initial={{ opacity: 0, y: 50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1 }}
-              >
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+          {projects.map((project, index) => (
+            <motion.div
+              key={index}
+              className="bg-[#1c2330] p-8 rounded-xl shadow-lg hover:shadow-2xl transition duration-300 flex flex-col justify-between"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              {/* Project Image */}
+              <div className="relative group mb-6">
                 <img
                   src={project.imageUrl}
                   alt={project.title}
-                  className="w-full h-48 object-cover rounded-lg mb-6"
+                  className="w-full h-48 object-cover rounded-lg shadow-lg filter grayscale group-hover:grayscale-0 transition duration-500"
                 />
-                <h3 className="text-2xl font-semibold text-[#6FFFE9] mb-2">
-                  {project.title}
-                </h3>
-                <p className="text-lg text-[#ccd6f6] mb-4">
-                  {project.description}
-                </p>
+              </div>
 
-                <div className="mt-4 mb-6">
-                  <span className="font-mono text-sm text-[#8892b0]">
-                    Tech Stack:{" "}
-                  </span>
-                  <ul className="flex flex-wrap gap-2 mt-2 text-sm text-[#8892b0]">
-                    {project.technologies.map((tech, techIndex) => (
-                      <li
-                        key={techIndex}
-                        className="before:content-['▹'] before:text-[#6FFFE9] before:mr-2"
-                      >
-                        {tech}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+              {/* Project Title */}
+              <h3 className="text-2xl font-semibold text-[#6FFFE9] mb-4">
+                {project.title}
+              </h3>
 
-                {/* Button with Padding Adjustment */}
+              {/* Project Description */}
+              <p className="text-lg text-[#ccd6f6] mb-6 leading-relaxed">
+                {project.description}
+              </p>
+
+              {/* Technologies */}
+              <div className="mb-6">
+                <span className="font-mono text-sm text-[#8892b0]">
+                  Tech Stack:
+                </span>
+                <ul className="grid grid-cols-2 gap-y-2 mt-2 text-sm text-[#8892b0]">
+                  {project.technologies.map((tech, techIndex) => (
+                    <li
+                      key={techIndex}
+                      className="before:content-['▹'] before:text-[#6FFFE9] before:mr-2"
+                    >
+                      {tech}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              {/* View GitHub Button */}
+              <div className="mt-auto">
                 <a
                   href={project.githubLink}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="absolute bottom-4 left-1/2 transform -translate-x-1/2 px-8 py-3 bg-[#0B132B] text-[#6FFFE9] text-sm font-medium rounded-lg hover:bg-[#6FFFE9] hover:text-[#0B132B] border-[#6FFFE9] border-2 transition duration-300"
+                  className="inline-block px-6 py-3 bg-[#0B132B] text-[#6FFFE9] text-sm font-medium rounded-lg border-2 border-[#6FFFE9] hover:bg-[#6FFFE9] hover:text-[#0B132B] transition duration-300"
                 >
                   View GitHub
                 </a>
-              </motion.div>
-            ))}
-          </div>
+              </div>
+            </motion.div>
+          ))}
         </div>
       </div>
     </section>
